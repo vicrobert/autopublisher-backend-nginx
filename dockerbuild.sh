857c55ce274114ede30429df0de9f710d728c415
vicrobert <you@example.com>
2020-12-06 19:36:56 +0800

    bugfix

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,7 +2,7 @@
 [ -e ./bin/md5 ] || {
     sudo mkdir -p ./bin
     sudo wget https://github.com/kelseyhightower/confd/releases/download/v0.16.0/confd-0.16.0-linux-amd64 -O ./bin/confd
-    sudo md5=`sudo md5sum ./bin/confd | awk '{print $1}'`
-    sudo echo ${md5} > ./bin/md5
+    md5=`sudo md5sum ./bin/confd | awk '{print $1}'`
+    sudo sh -c "echo ${md5} > ./bin/md5"
 }
 sudo docker build -t autopublisher-backend-nginx .
